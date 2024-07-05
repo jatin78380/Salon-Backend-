41c4637b370fce720a49afcc64c53a460f8fd95d
jatinssh <jatin7838066@gmail.com>
2024-07-05 20:04:14 +0530

    updated

--- backend/models/StylistModel.js
+++ backend/models/StylistModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://jatin8612:Jatin%40rkblbrrk!12@cluster0.nkvwqm9.mongodb.net/saloninternship");
+mongoose.connect("mongodurl");
 // Define the selection schema
 const selectionSchema = new mongoose.Schema({
     options: [String], // Array of selected options
