240b6b3010961ee2eb72a3d1543e09e2b04c153c
jatinssh <jatin7838066@gmail.com>
2024-07-26 20:44:35 +0530

    updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const PORT = 3001;
-//twilio
+
 
 
 
@@ -40,38 +40,7 @@ try {
 } catch (e) {
   console.log("Error occured while configuring cloudinary.");
 }
-app.post('/send-otp', async (req, res) => {
-  const { phoneNumber } = req.body; // Phone number in E.164 format
 
-  try {
-    const verification = await client.verify.services(twilio.serviceSid)
-      .verifications
-      .create({ to: phoneNumber, channel: 'sms' });
-
-    res.status(200).json({ message: 'OTP sent successfully', verificationSid: verification.sid });
-  } catch (error) {
-    console.error('Error sending OTP:', error);
-    res.status(500).json({ message: 'Error sending OTP' });
-  }
-});
-app.post('/verify-otp', async (req, res) => {
-  const { phoneNumber, otp } = req.body; // Phone number and OTP
-  
-  try {
-    const verificationCheck = await client.verify.services(twilio.serviceSid)
-      .verificationChecks
-      .create({ to: phoneNumber, code: otp });
-
-    if (verificationCheck.status === 'approved') {
-      res.status(200).json({ message: 'OTP verified successfully' });
-    } else {
-      res.status(400).json({ message: 'Invalid OTP' });
-    }
-  } catch (error) {
-    console.error('Error verifying OTP:', error);
-    res.status(500).json({ message: 'Error verifying OTP' });
-  }
-});
 app.use("/service", service);
 app.use("/customer", customerrouter);
 app.use("/stylist", stylistrouter);
